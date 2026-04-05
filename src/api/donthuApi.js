b814c964d74af7dee696c6942a0d2035d1cd6c76
VanMinhNguyen21 <vanminhnguyen00@gmail.com>
2026-04-05 16:20:28 +0700

    update code

---
+++ src/api/donthuApi.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8000/api/donthu';
+
+export const donthuApi = {
+    async getDonThus(params = {}) {
+        const response = await axios.get(BASE_URL, { params });
+        return response.data;
+    },
+    async getDonThu(id) {
+        const response = await axios.get(`${BASE_URL}/${id}`);
+        return response.data;
+    },
+    async createDonThu(data) {
+        const response = await axios.post(BASE_URL, data);
+        return response.data;
+    },
+    async updateDonThu(id, data) {
+        const response = await axios.put(`${BASE_URL}/${id}`, data);
+        return response.data;
+    },
+    async deleteDonThu(id) {
+        const response = await axios.delete(`${BASE_URL}/${id}`);
+        return response.data;
+    }
+};
