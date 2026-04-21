393c22b3656dbb107aed698beb5863415777af3d
VanMinhNguyen21 <vanminhnguyen00@gmail.com>
2026-04-21 08:27:46 +0700

    update code

--- src/api/assignmentApi.js
+++ src/api/assignmentApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api/nguon_tin';
-// const BASE_URL = 'https://backend-p-c-1.vercel.app/api/api/nguon_tin';
+// const BASE_URL = 'http://localhost:8000/api/nguon_tin';
+const BASE_URL = 'https://backend-p-c-1.vercel.app/api/api/nguon_tin';
     
 export const assignmentApi = {
     /**
